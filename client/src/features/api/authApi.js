10869ba45b0a1e40d2cf694af62efc3eee3a1037
Ratnojit Saha <saharatnojit04@gmail.com>
2025-03-21 12:07:27 +0530

    bug fixing

--- client/src/features/api/authApi.js
+++ client/src/features/api/authApi.js
@@ -1,7 +1,7 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import { userLoggedIn, userLoggedOut } from "../authSlice";
 
-const USER_API = "https://lms-mern-b3uf.onrender.com"
+const USER_API = "https://lms-mern-b3uf.onrender.com";
 
 export const authApi = createApi({
     reducerPath:"authApi",
