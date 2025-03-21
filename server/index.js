b2bf1b2b5863b130bbd277bc16ab49869792b3db
Ratnojit Saha <110903082+ratnojitsaha@users.noreply.github.com>
2025-03-21 12:56:32 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:["http://localhost:5173" , "https://lms-mern-frontend-oolh.onrender.com/"],
+    origin:"https://lms-mern-frontend-oolh.onrender.com/",
     credentials:true
 }));
  
