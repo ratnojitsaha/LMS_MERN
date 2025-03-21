2d6b5706bd84963a08f774316ebf33af64728fc8
Ratnojit Saha <110903082+ratnojitsaha@users.noreply.github.com>
2025-03-21 10:40:10 +0530

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -53,6 +53,7 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+    plugins: [tailwindcssAnimate],
+    import tailwindcssAnimate from "tailwindcss-animate";
 }
 
