6d40865835c7b8603516a4276d308f75d77f869a
Ratnojit Saha <saharatnojit04@gmail.com>
2025-04-24 19:35:11 +0530

    update
    none

--- client/src/features/api/authApi.js
+++ client/src/features/api/authApi.js
@@ -1,7 +1,7 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import { userLoggedIn, userLoggedOut } from "../authSlice";
 
-
+    // backend url/api/v1/user
 const USER_API = "https://lms-mern-b3uf.onrender.com/api/v1/user/";
 
 export const authApi = createApi({
