5cf4b5c003c5de2bb3f02b52efb6ced7927ed293
Ratnojit Saha <saharatnojit04@gmail.com>
2025-04-29 11:23:16 +0530

    resolving more and more bugs

--- server/utils/multer.js
+++ server/utils/multer.js
@@ -1,12 +1,23 @@
-// import multer from "multer";
-
-// const upload = multer({dest:"uploads/"});
-// export default upload
-
-
+import fs from "fs";
 import multer from "multer";
+import path from "path";
+import os from "os";
 
-const storage = multer.memoryStorage(); // store files in memory
+const tempDir = path.join(os.tmpdir(), "uploads");
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const tempDir = path.join(os.tmpdir(), "uploads");
+    cb(null, tempDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
 const upload = multer({ storage });
 
 export default upload;
