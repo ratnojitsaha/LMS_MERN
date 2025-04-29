cc5dfee46ea1f11f341268c30b8a76500b107462
Ratnojit Saha <saharatnojit04@gmail.com>
2025-04-29 11:12:57 +0530

    fixing the bug with cors
    Access to fetch at 'https://lms-mern-v2.vercel.app/api/v1/user/profile' from origin 'https://lms-mern-k94l.vercel.app' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.Understand this error
    lms-mern-v2.vercel.app/api/v1/user/profile:1
    
               Failed to load resource: net::ERR_FAILEDUnderstand this error
    index-D-7ohTxm.js:77 Object
    lms-mern-k94l.vercel.app/:1 Access to fetch at 'https://lms-mern-v2.vercel.app/api/v1/user/profile' from origin 'https://lms-mern-k94l.vercel.app' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.Understand this error
    lms-mern-v2.vercel.app/api/v1/user/profile:1
    
               Failed to load resource: net::ERR_FAILED

--- server/index.js
+++ server/index.js
@@ -27,11 +27,21 @@ app.use(cookieParser());
 //     origin:"http://localhost:5173",
 //     credentials:true
 // }));
-app.use(cors({
-     origin: FRONT_URL,
-    // origin : "*",
-    credentials:true
-}));
+// app.use(cors({
+//      origin: FRONT_URL,
+//     // origin : "*",
+//     credentials:true
+// }));
+
+
+const corsOptions = {
+    origin: 'https://lms-mern-k94l.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials : true
+  };
+  
+app.use(cors(corsOptions));
  
 // apis
 app.use("/api/v1/media", mediaRoute);
